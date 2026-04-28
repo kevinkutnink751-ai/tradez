b52dbf34d28264dd793bd80d3c44d22d2560f483
Anonymous User <golobal@gmail.com>
2026-04-28 17:20:26 +0100

    feat: force HTTPS in production and include package discovery in post-autoload-dump scripts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ use App\Models\TermsPrivacy;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage as FacadesStorage;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -36,19 +37,22 @@ class AppServiceProvider extends ServiceProvider
             return new Filesystem(new SftpAdapter($config));
         });
 
-        Paginator::useBootstrap();
-        if (!app()->runningInConsole() && app()->environment('production')) {
-          // Sharing settings with all view
-            $settings = Settings::where('id', '1')->first();
-            $terms =  TermsPrivacy::find(1);
-            $moreset =  SettingsCont::find(1);
-            $bg =  SettingsCont::find(1)->theme;
-
-            View::share('settings', $settings);
-            View::share('terms', $terms);
-            View::share('moresettings', $moreset);
-            View::share('mod', $settings->modules);
-            View::share('bg', $bg);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
+
+        Paginator::useBootstrap();
+        
+        // Sharing settings with all view
+        $settings = Settings::where('id', '1')->first();
+        $terms =  TermsPrivacy::find(1);
+        $moreset =  SettingsCont::find(1);
+        $bg =  SettingsCont::find(1)->theme;
+
+        View::share('settings', $settings);
+        View::share('terms', $terms);
+        View::share('moresettings', $moreset);
+        View::share('mod', $settings->modules);
+        View::share('bg', $bg);
     }
 }
